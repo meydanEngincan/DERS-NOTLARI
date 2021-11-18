46df97203c12cda697d4efcb81e341611720d553
meydanEngincan <engincanmeydan95@gmail.com>
2021-11-18 08:32:57 +0300

    Metot ekleme yapıldı

--- DERS2-Operators/Ders7-MetotSoruları/Program.cs
+++ DERS2-Operators/Ders7-MetotSoruları/Program.cs
@@ -12,51 +12,58 @@ namespace Ders7_MetotSoruları
 
         // ÖNEMLİ NOT!!! = Metodun içinde kullanıcıdan veri isteme.
         // ÖNEMLİ NOT2!!! = METOT İÇİNDE YAZDIRMA
-        static int KullaniciIslem(int s1,int s2,string islem)
-        {
-            int sonuc = 0;
+        //static int KullaniciIslem(int s1,int s2,string islem)
+        //{
+        //    int sonuc = 0;
 
-            if (islem.ToLower() == "topla")
-            {
-                sonuc = s1 + s2;                
-                //Console.WriteLine(sonuc);
-                return sonuc;
-            }
-            else if (islem.ToLower() == "çıkart")
-            {
-                sonuc = s1 - s2;               
-                //Console.WriteLine(sonuc);
-                return sonuc;
-            }
-            else if (islem.ToLower() == "çarp")
-            {
-                sonuc = s1 * s2;              
-                //Console.WriteLine(sonuc);
-                return sonuc;
-            }
-            else if (islem.ToLower()=="böl")
-            {
-                sonuc = s1 / s2;               
-                //Console.WriteLine(sonuc);
-                return sonuc;
-            }
-            return sonuc;
-        }
+        //    if (islem.ToLower() == "topla")
+        //    {
+        //        sonuc = s1 + s2;                
+        //        //Console.WriteLine(sonuc);
+        //        return sonuc;
+        //    }
+        //    else if (islem.ToLower() == "çıkart")
+        //    {
+        //        sonuc = s1 - s2;               
+        //        //Console.WriteLine(sonuc);
+        //        return sonuc;
+        //    }
+        //    else if (islem.ToLower() == "çarp")
+        //    {
+        //        sonuc = s1 * s2;              
+        //        //Console.WriteLine(sonuc);
+        //        return sonuc;
+        //    }
+        //    else if (islem.ToLower()=="böl")
+        //    {
+        //        sonuc = s1 / s2;               
+        //        //Console.WriteLine(sonuc);
+        //        return sonuc;
+        //    }
+        //    return sonuc;
+        //}
         static void Main(string[] args)
         {
-            //SORU= Kullanıcıdan 2 sayı 1 işlem alıp sonucu döndüren metot tanımlayınız.
-            Console.Write("Bir Sayı Giriniz: ");
-            int s1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bir Sayı Giriniz: ");
-            int s2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Bir İşlem Türü Giriniz: ");
-            string isleminiz = Console.ReadLine();
+            //string selam = MerhabaDondur();
+            //Console.WriteLine(selam);
+            //    //SORU= Kullanıcıdan 2 sayı 1 işlem alıp sonucu döndüren metot tanımlayınız.
+            //    Console.Write("Bir Sayı Giriniz: ");
+            //    int s1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Bir Sayı Giriniz: ");
+            //    int s2 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Bir İşlem Türü Giriniz: ");
+            //    string isleminiz = Console.ReadLine();
 
-            int a = KullaniciIslem(s1, s2, isleminiz);
-            Console.WriteLine(a);
-
+            //    int a = KullaniciIslem(s1, s2, isleminiz);
+            //    Console.WriteLine(a);
+        }
             //SORU= Kendisine gönderilen float dizisini double dizisine çeviren makeItDouble isimli metodu tanımlayınız tanımlayınız 10 elemanlı
 
-        }
+        //    static string MerhabaDondur()
+        //{
+            //string metin = "Merhaba";
+            //return metin;
+            //return "Merhaba"; hem üstte ki hem altta ki şekilde yapılabilir.
+        
     }
 }
